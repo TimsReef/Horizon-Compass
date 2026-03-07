b041e8656bcf2420081f65ea088e7c3012bd5150
TimsReef <tgizinski@outlook.com>
2026-03-07 13:38:30 -0500

    Update withAndroidAuto.js

--- withAndroidAuto.js
+++ withAndroidAuto.js
@@ -1,7 +1,9 @@
-const { withAndroidManifest } = require('@expo/config-plugins');
+const { withAndroidManifest, withDangerousMod } = require('@expo/config-plugins');
+const fs = require('fs');
+const path = require('path');
 
 module.exports = function withAndroidAuto(config) {
-  return withAndroidManifest(config, async (config) => {
+  config = withAndroidManifest(config, async (config) => {
     const androidManifest = config.modResults;
     const application = androidManifest.manifest.application[0];
 
@@ -25,4 +27,26 @@ module.exports = function withAndroidAuto(config) {
 
     return config;
   });
+
+  config = withDangerousMod(config, [
+    'android',
+    async (config) => {
+      const resDir = path.join(config.modRequest.platformProjectRoot, 'app', 'src', 'main', 'res', 'xml');
+      if (!fs.existsSync(resDir)) {
+        fs.mkdirSync(resDir, { recursive: true });
+      }
+
+      const automotiveAppDescPath = path.join(resDir, 'automotive_app_desc.xml');
+      const automotiveAppDescContent = `<?xml version="1.0" encoding="utf-8"?>
+<automotiveApp>
+  <uses name="template" />
+</automotiveApp>
+`;
+
+      fs.writeFileSync(automotiveAppDescPath, automotiveAppDescContent);
+      return config;
+    },
+  ]);
+
+  return config;
 };
